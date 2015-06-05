40b48a115be958b3acfbca9c444e7a3be4e9f896
Michał Hartwich <michal.hartwich@gmail.com>
2015-06-05 16:41:08 +0200

    Change Categories controller to pass tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
+  before_action :authenticate_user!, :authenticate_admin!, 
+    only: [:new, :edit, :update, :destroy, :create]
   
   expose(:categories)
   expose(:category)
@@ -43,5 +44,11 @@ class CategoriesController < ApplicationController
   private
     def category_params
       params.require(:category).permit(:name)
+    end
+
+    def authenticate_admin!
+      unless current_user.try(:admin?)
+        redirect_to(new_user_session_path)
+      end
     end
 end
