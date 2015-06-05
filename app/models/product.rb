5ba357873699dc4601b58109967a05a31b1b8f02
Michał Hartwich <michal.hartwich@gmail.com>
2015-06-05 16:48:51 +0200

    Change Product model to pass tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :title, :description, :price, presence: true
+  validates :price, format: { with: /\A[0-9]{1,}\.\d{1,2}\Z/ }
+
+  def average_rating
+    reviews.average(:rating).round(2)
+  end
 end
