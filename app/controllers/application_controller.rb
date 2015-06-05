ae0cd640e1b0da57bea5989ff3834fb406c1be39
Michał Hartwich <michal.hartwich@gmail.com>
2015-06-05 17:27:58 +0200

    Fix Devise sign up issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   add_flash_types :error
 
   decent_configuration do
@@ -7,4 +8,10 @@ class ApplicationController < ActionController::Base
   end
 
   protect_from_forgery with: :exception
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :firstname, 
+        :lastname, :password, :password_confirmation) }
+    end
 end
