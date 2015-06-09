58c60e7c17ee94b134041c9a638301d660905ab2
Michał Hartwich <michal.hartwich@gmail.com>
2015-06-09 20:36:40 +0200

    Fix Product-Review model dependancy

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   validates :title, :description, :price, presence: true
   validates :price, format: { with: /\A[0-9]{1,}\.\d{1,2}\Z/ }
