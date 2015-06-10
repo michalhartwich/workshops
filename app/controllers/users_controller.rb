a33c057b2c9f66046ef5a7ed543588bc0fe20fba
Michał Hartwich <michal.hartwich@gmail.com>
2015-06-10 20:29:29 +0200

    Limit review count to 5 in User profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   expose(:user)
-  expose_decorated(:reviews, ancestor: :user) { |r| r.order('created_at DESC') }
+  expose_decorated(:reviews, ancestor: :user) { |r| r.order('created_at DESC').limit(5) }
 end
