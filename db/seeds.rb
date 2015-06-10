bfa8283d9cf7e6055511544a484bcd082ec02790
Michał Hartwich <michal.hartwich@gmail.com>
2015-06-10 19:58:49 +0200

    Fill seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,33 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(firstname: 'John', lastname: 'Doe', email: 'john.doe@example.com', password: 'dontknow',
+admin = User.create(firstname: 'John', lastname: 'Doe', email: 'john.doe@example.com', password: 'dontknow',
   password_confirmation: 'dontknow', image_id: 'eaywz1u3boxdk5ujwiau', admin: true)
 
-User.create(firstname: 'Cary', lastname: 'Adkins', email: 'cary.adkins@example.com', password: 'dontknow',
+cary = User.create(firstname: 'Cary', lastname: 'Adkins', email: 'cary.adkins@example.com', password: 'dontknow',
   password_confirmation: 'dontknow')
 
-User.create(firstname: 'Essie', lastname: 'Foster', email: 'essie.foster@example.com', password: 'dontknow',
+essie = User.create(firstname: 'Essie', lastname: 'Foster', email: 'essie.foster@example.com', password: 'dontknow',
   password_confirmation: 'dontknow')
 
-User.create(firstname: 'Ron', lastname: 'Jones', email: 'ron.jones@example.com', password: 'dontknow',
+ron = User.create(firstname: 'Ron', lastname: 'Jones', email: 'ron.jones@example.com', password: 'dontknow',
   password_confirmation: 'dontknow')
 
-User.create(firstame: 'Kay', lastname: 'Holt', email: 'kay.holt@example.com', password: 'dontknow',
+kay = User.create(firstname: 'Kay', lastname: 'Holt', email: 'kay.holt@example.com', password: 'dontknow',
   password_confirmation: 'dontknow')
+
+kristin = User.create(firstname: 'Kristin', lastname: 'Page', email: 'kristin.page@example.com', password: 'dontknow',
+  password_confirmation: 'dontknow')
+
+books = Category.create(name: 'Books')
+
+product_1 = Product.create(title: 'Wolves Of Tomorrow', description: 'It is a great story about Wolves.', price: 15, 
+    category: books, user_id: cary.id)
+product_2 = Product.create(title: 'Vision Of Twilight', description: 'This story treats about apocalypse', price: 13, 
+    category: books, user: essie)
+
+review_1 = Review.create(content: 'Great story!', rating: 5, user: ron, product: product_1)
+review_2 = Review.create(content: 'Very laaazy. I don\'t recommend.', rating: 2, user: kay, product: product_1)
+
+review_3 = Review.create(content: 'It is very good item for gift.', rating: 4, user: kristin, product: product_2)
+review_4 = Review.create(content: "It's really good!", rating: 5, user: essie, product: product_2)
