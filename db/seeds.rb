09b4bef2ae8dd4964ad79f45f3b97540975aa321
Michał Hartwich <michal.hartwich@gmail.com>
2015-06-10 13:22:33 +0200

    Heroku deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(firstname: 'John', lastname: 'Doe', email: 'john.doe@example.com', password: 'dontknow',
+  password_confirmation: 'dontknow', image_id: 'eaywz1u3boxdk5ujwiau', admin: true)
+
+User.create(firstname: 'Cary', lastname: 'Adkins', email: 'cary.adkins@example.com', password: 'dontknow',
+  password_confirmation: 'dontknow')
+
+User.create(firstname: 'Essie', lastname: 'Foster', email: 'essie.foster@example.com', password: 'dontknow',
+  password_confirmation: 'dontknow')
+
+User.create(firstname: 'Ron', lastname: 'Jones', email: 'ron.jones@example.com', password: 'dontknow',
+  password_confirmation: 'dontknow')
+
+User.create(firstame: 'Kay', lastname: 'Holt', email: 'kay.holt@example.com', password: 'dontknow',
+  password_confirmation: 'dontknow')
