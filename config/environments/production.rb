68588902aede3ce3710e3d541f7c249b947b784b
Michał Hartwich <michal.hartwich@gmail.com>
2015-06-10 17:46:50 +0200

    Heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,13 +87,13 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.mandrillapp.com",
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    domain: ENV["MANDRILL_DOMAIN"],
     authentication: 'plain',
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["MANDRILL_USERNAME"],
+    password: ENV["MANDRILL_APIKEY"]
   }
 
 end
